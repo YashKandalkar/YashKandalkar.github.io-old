2f4d7301dfd921044dc48242fbf7aa5b9696bd72
Yash Kandalkar <yashsk.yk@gmail.com>
2020-06-14 23:57:27 +0530

    Updates

---
+++ precache-manifest.84675e04097eda336b662f01089503bc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a7d883b6898a4a7e19b010da9a1253",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe00ec969a92acbd0092",
+    "url": "/static/css/main.612c7bc9.chunk.css"
+  },
+  {
+    "revision": "901c038dd0a36cdb350a",
+    "url": "/static/js/2.d1574fe2.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.d1574fe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe00ec969a92acbd0092",
+    "url": "/static/js/main.7cdaa2c6.chunk.js"
+  },
+  {
+    "revision": "4a58f07abc3c608b88a0",
+    "url": "/static/js/runtime-main.118059e6.js"
+  },
+  {
+    "revision": "de8edcc1e39f2bbd43d085709cc9e2c7",
+    "url": "/static/media/canary.de8edcc1.png"
+  },
+  {
+    "revision": "51d34a7cef6b5b94020cb888a9adcee9",
+    "url": "/static/media/chatitdude.51d34a7c.png"
+  },
+  {
+    "revision": "53be63de32eb7bd601d00618221d6dab",
+    "url": "/static/media/tachyons.53be63de.png"
+  },
+  {
+    "revision": "c8d13ad98145020c17925e3c409d0d0c",
+    "url": "/static/media/tictactoe.c8d13ad9.png"
+  }
+]);
