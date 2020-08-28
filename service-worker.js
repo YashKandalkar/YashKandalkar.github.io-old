c8d981a97c4f1f4206a87a431f03637662644666
YashKandalkar <yashsk.yk@gmail.com>
2020-08-28 16:09:48 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30dfb872e59b762d4eb317ed32505121.js"
+  "/precache-manifest.4e3bb9e9267d86ffdcb4cdfa11ac52fa.js"
 );
 
 self.addEventListener('message', (event) => {
